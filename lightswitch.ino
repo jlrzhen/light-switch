730329aa03ad84385a0be7e215c352500e6f68b2
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-05-05 22:59:15 -0400

    disconnect servo after moving to eliminate vibration noise

--- lightswitch.ino
+++ lightswitch.ino
@@ -1,6 +1,16 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
 int upper = 140;
@@ -8,43 +18,48 @@ int lower = 90;
 int mid = (upper+lower)/2;
 
 void setup() {
-  myservo.attach(8);  // attaches the servo on pin 8 to the servo object
   resetServo();
   Serial.begin(9600);
 }
 
 void lightOn() {
-  for (pos = mid; pos <= upper; pos += 1) {
+  myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+  for (pos = mid; pos <= upper; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     Serial.println(pos);
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
+  myservo.detach();
 }
 
 void lightOff() {
-  for (pos = mid; pos >= lower; pos -= 1) {
+  myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+  for (pos = mid; pos >= lower; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     Serial.println(pos);
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
+  myservo.detach();
 }
 
 void resetServo() {
+  myservo.attach(8);  // attaches the servo on pin 9 to the servo object
   if(pos > mid) {
-    for (pos = upper; pos >= mid; pos -= 1) {
+    for (pos = upper; pos >= mid; pos -= 1) { // goes from 180 degrees to 0 degrees
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       Serial.println(pos);
       delay(15);                       // waits 15 ms for the servo to reach the position
     }
   } else {
-    for (pos = lower; pos <= mid; pos += 1) {
+    for (pos = lower; pos <= mid; pos += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       Serial.println(pos);
       delay(15);                       // waits 15 ms for the servo to reach the position
     }
   }
+  myservo.detach();
 }
 
 void loop() {
